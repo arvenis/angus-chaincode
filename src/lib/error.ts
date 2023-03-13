fec111892910399f183b50f39632c4b2801118a8
Gyorgy Zsapka <gyorgy.zsapka@gmail.com>
2023-03-13 11:01:46 +0000

    feat: add BAD_REQUEST error code and optional error message input for the AngusChaincodeError constructor

--- src/lib/error.ts
+++ src/lib/error.ts
@@ -3,21 +3,22 @@ export enum AngusErrorCodes {
     INTERNAL_ERROR,
     INVALID_SERVICE,
     ENTITY_ALREADY_EXISTS,
-    ENTITY_NOT_EXIST
-};
+    ENTITY_NOT_EXIST,
+}
 
-const ERRORMESSAGE:string[] = [
-    "|ACCESS_DENIED|The access is denied for the user (authorization error)|",
+const ERRORMESSAGE: string[] = [
+    '|ACCESS_DENIED|The access is denied for the user (authorization error)|',
     "|INTERNAL_ERROR|We don't know exactly what happened... Check the logs and good luck!|",
     "|INVALID_SERVICE|The requested chaincode service doesn't exist|",
-    "|ENTITY_ALREADY_EXISTS|Entity already exists|",
-    "|ENTITY_NOT_EXIST|The entity does not exist|"
+    '|ENTITY_ALREADY_EXISTS|Entity already exists|',
+    '|ENTITY_NOT_EXIST|The entity does not exist|',
+    "|BAD_REQUEST|The request can't be processed due to a client error|",
 ];
 
 export class AngusChaincodeError extends Error {
     code: AngusErrorCodes;
 
-    constructor(code: AngusErrorCodes) {
-        super(ERRORMESSAGE[code]);
+    constructor(code: AngusErrorCodes, message?: string) {
+        super(ERRORMESSAGE[code] + message);
     }
 }
