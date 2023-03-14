adc0dcec3b2c12c4868471b9c66f102581463cef
Gyorgy Zsapka <gyorgy.zsapka@gmail.com>
2023-03-14 12:25:04 +0000

    fix: fix the BAD_REQUEST code definition
    + replace the message with the given one instead of simple adding it to the end

--- src/lib/error.ts
+++ src/lib/error.ts
@@ -1,24 +1,32 @@
 export enum AngusErrorCodes {
     ACCESS_DENIED,
     INTERNAL_ERROR,
+    BAD_REQUEST,
     INVALID_SERVICE,
     ENTITY_ALREADY_EXISTS,
     ENTITY_NOT_EXIST,
 }
 
+// Order should be the same as in the AngusErrorCodes above
 const ERRORMESSAGE: string[] = [
     '|ACCESS_DENIED|The access is denied for the user (authorization error)|',
     "|INTERNAL_ERROR|We don't know exactly what happened... Check the logs and good luck!|",
+    "|BAD_REQUEST|The request can't be processed due to a client error|",
     "|INVALID_SERVICE|The requested chaincode service doesn't exist|",
     '|ENTITY_ALREADY_EXISTS|Entity already exists|',
     '|ENTITY_NOT_EXIST|The entity does not exist|',
-    "|BAD_REQUEST|The request can't be processed due to a client error|",
 ];
 
 export class AngusChaincodeError extends Error {
     code: AngusErrorCodes;
 
     constructor(code: AngusErrorCodes, message?: string) {
-        super(ERRORMESSAGE[code] + message);
+        let errorMessage = ERRORMESSAGE[code];
+        if (message) {
+            // Replace the error message for the given one.
+            const originalMessage = message.split('|');
+            errorMessage = ['', originalMessage[1], message, ''].join('|');
+        }
+        super(errorMessage);
     }
 }
